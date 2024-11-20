2bd15d74a12f0e3ef5ad31cb47e2dce9f67674c8
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-11-20 17:14:50 -0300

    feat(api): create database structure and create account route

---
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -0,0 +1,48 @@
+import { hash } from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function createAccount(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/users',
+    {
+      schema: {
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+          password: z.string().min(6),
+        }),
+      },
+    },
+    async (req, res) => {
+      const { email, name, password } = req.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        return res.status(400).send({
+          message: 'User with same e-mail already exists',
+        })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return res.status(201).send()
+    },
+  )
+}
