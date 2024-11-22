166ca02959d69460256677d8969396510fe2906b
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-11-22 10:21:12 -0300

    feat: create "get organization" route

---
+++ apps/api/src/http/routes/orgs/get-organization.ts
@@ -0,0 +1,67 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+export async function getOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get details from organization',
+          params: z.object({
+            slug: z.string(),
+          }),
+          security: [{ bearerAuth: [] }],
+          response: {
+            200: z.object({
+              organization: z.object({
+                name: z.string(),
+                id: z.string(),
+                slug: z.string(),
+                domain: z.string().nullable(),
+                shouldAttachUsersByDomain: z.boolean(),
+                avatarUrl: z.string().url().nullable(),
+                createdAt: z.date(),
+                updatedAt: z.date(),
+                ownerId: z.string(),
+              }),
+            }),
+          },
+        },
+      },
+      async (req, res) => {
+        const userId = await req.getCurrentUserId()
+
+        const user = await prisma.user.findUnique({
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatarUrl: true,
+          },
+          where: {
+            id: userId,
+          },
+        })
+
+        if (!user) {
+          throw new Error('User not found.')
+        }
+
+        const { slug } = req.params
+
+        const { organization } = await req.getUserMembership(slug)
+
+        return res.status(200).send({
+          organization,
+        })
+      },
+    )
+}
