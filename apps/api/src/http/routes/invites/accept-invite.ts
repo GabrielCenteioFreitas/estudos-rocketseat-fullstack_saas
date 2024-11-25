c6469ffd1d9d6d0a7d704b6933a8aef08722c300
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-11-25 16:36:43 -0300

    feat: create "accept invite" route

---
+++ apps/api/src/http/routes/invites/accept-invite.ts
@@ -0,0 +1,76 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function acceptInvite(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/invites/:inviteId/accept',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'Accept an invite',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            inviteId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (req, res) => {
+        const userId = await req.getCurrentUserId()
+
+        const user = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+        })
+
+        if (!user) {
+          throw new BadRequestError('User not found.')
+        }
+
+        const { inviteId } = req.params
+
+        const invite = await prisma.invite.findUnique({
+          where: {
+            id: inviteId,
+          },
+        })
+
+        if (!invite) {
+          throw new BadRequestError('Invite not found or expired.')
+        }
+
+        if (invite.email !== user.email) {
+          throw new BadRequestError('This invite belongs to another user.')
+        }
+
+        await prisma.$transaction([
+          prisma.member.create({
+            data: {
+              userId,
+              organizationId: invite.organizationId,
+              role: invite.role,
+            },
+          }),
+          prisma.invite.delete({
+            where: {
+              id: inviteId,
+            },
+          }),
+        ])
+
+        return res.status(204).send()
+      },
+    )
+}
