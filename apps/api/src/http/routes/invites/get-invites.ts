e1289bdff72e75ec85ed341dbb6773cc98d1c758
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-11-25 16:24:39 -0300

    feat: create "get invites" route

---
+++ apps/api/src/http/routes/invites/get-invites.ts
@@ -0,0 +1,102 @@
+import { roleSchema } from '@saas/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function getInvites(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/invites',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'Get all organization invites',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              invites: z.array(
+                z.object({
+                  id: z.string().uuid(),
+                  email: z.string().email(),
+                  role: roleSchema,
+                  createdAt: z.date(),
+                  author: z
+                    .object({
+                      id: z.string().uuid(),
+                      name: z.string().nullable(),
+                    })
+                    .nullable(),
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (req, res) => {
+        const userId = await req.getCurrentUserId()
+
+        const user = await prisma.user.findUnique({
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatarUrl: true,
+          },
+          where: {
+            id: userId,
+          },
+        })
+
+        if (!user) {
+          throw new Error('User not found.')
+        }
+
+        const { slug } = req.params
+        const { membership, organization } = await req.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('get', 'Invite')) {
+          throw new UnauthorizedError(
+            'You are not allowed to get these organization invites.',
+          )
+        }
+
+        const invites = await prisma.invite.findMany({
+          where: {
+            organizationId: organization.id,
+          },
+          select: {
+            id: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            author: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        })
+
+        return res.status(200).send({
+          invites,
+        })
+      },
+    )
+}
