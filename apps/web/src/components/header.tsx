82d33f4c1eef876538238e1bc94f79d70a9a30f5
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-12-19 00:00:04 -0300

    fix: build issues

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -4,7 +4,11 @@ import Image from 'next/image'
 import rocketseatIcon from '@/assets/rocketseat-icon.svg'
 import { ability } from '@/auth/auth'
 
+import { OrganizationSwitcher } from './organization-switcher'
 import { PendingInvites } from './pending-invites'
+import { ProfileButton } from './profile-button'
+import { ProjectSwitcher } from './project-switcher'
+import { ThemeSwitcher } from './theme/theme-switcher'
 import { Separator } from './ui/separator'
 
 export const Header = async () => {
@@ -21,14 +25,12 @@ export const Header = async () => {
 
         <Slash className="size-3 -rotate-[24deg] text-border" />
 
-        {/* The DropdownMenu is causing an error, so for now, this component will remain commented out */}
-        {/* <OrganizationSwitcher /> */}
+        <OrganizationSwitcher />
 
         {permissions?.can('get', 'Project') && (
           <>
             <Slash className="size-3 -rotate-[24deg] text-border" />
-            {/* The DropdownMenu is causing an error, so for now, this component will remain commented out */}
-            {/* <ProjectSwitcher /> */}
+            <ProjectSwitcher />
           </>
         )}
       </div>
@@ -36,13 +38,11 @@ export const Header = async () => {
       <div className="flex items-center gap-4">
         <PendingInvites />
 
-        {/* The DropdownMenu is causing an error, so for now, this component will remain commented out */}
-        {/* <ThemeSwitcher /> */}
+        <ThemeSwitcher />
 
         <Separator orientation="vertical" className="h-5" />
 
-        {/* The DropdownMenu is causing an error, so for now, this component will remain commented out */}
-        {/* <ProfileButton /> */}
+        <ProfileButton />
       </div>
     </div>
   )
